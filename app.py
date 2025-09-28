d9ee6e163626be67bbc334e0039c547d94089c89
P.Jaswanth Krishna <jaswanthuchiha69@gmail.com>
2025-09-28 22:54:58 +0530

    removed space between country code

--- app.py
+++ app.py
@@ -59,9 +59,9 @@ def send_alert(table: str, row: dict):
     
     # Determine which phone number to use based on table
     if table in ["water_levels", "water_levels2"]:
-        recipient_phone = "+91 8096191416"
+        recipient_phone = "+918096191416"
     else:  # water_levels3, water_levels4
-        recipient_phone = "+91 7601024711"
+        recipient_phone = "+917601024711"
     
     message = f"""
     🚨 Anomaly Detected!
